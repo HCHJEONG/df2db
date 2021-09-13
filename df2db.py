8289bdb9307b7a1a1ccd792585290c22d31adc35
HCHJEONG <hcjeong@kakao.com>
2021-09-13 09:51:32 +0900

    trv

--- df2db.py
+++ df2db.py
@@ -308,6 +308,8 @@ if __name__ == "__main__" :
         df = pd.read_pickle(urls[i]) 
         print(df.columns.tolist())
         df=df.loc[ (df["decision_items"] != 0) | (df["decision_gists"] != 0) | (df["main_decision"] != 0) | (df["reasoning"] != 0), : ] 
+        # df = df.loc[(df['reasoning']!=""), :]
+        df = df.replace({'reasoing': ''}, {'reasoning': 0})
         
         init_db()
         limit = 1000000000
@@ -317,6 +319,8 @@ if __name__ == "__main__" :
     for i in range(len(urls)):
         df = pd.read_pickle(urls[i])
         print(df.columns.tolist())
+        # df = df.loc[(df['reasoning']!=""), :]
+        df = df.replace({'reasoing': ''}, {'reasoning': 0})
         
         init_db()
         limit = 1000000000
